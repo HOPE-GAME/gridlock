dee197d746c99d1e3ce82139261f129606673b79
HOPE-GAME <rosseja031@ignatius.sa.edu.au>
2026-06-09 17:51:10 +0930

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // Room state
 const rooms = {};
